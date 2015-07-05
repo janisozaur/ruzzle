d57e57c4867152aa6975be0cf543bdccd0be77f7
Michał Janiszewski <janisozaur@gmail.com>
2015-07-05 21:33:25 +0200

    wordazzle in Golang. board population so far.

--- wordazzle.cpp
+++ wordazzle.cpp
@@ -126,116 +126,6 @@ bool verify_word(const vector<char> &grid, const unsigned int &w,
 	return false;
 }
 
-struct GridResult
-{
-	bool valid;
-	vector<char> grid;
-};
-
-struct RNGBase {
-	virtual float operator()() = 0;
-	virtual ~RNGBase() {};
-};
-
-template< class RNG >
-struct SmartRNG : RNGBase {
-	RNG gen;
-
-	SmartRNG(RNG &gen) : gen(gen) {}
-	virtual float operator()() {
-		uniform_real_distribution<float> distribution(0.f, 1.f);
-		return distribution(gen);
-	}
-};
-
-char sample_letter(const vector<pair_lf> letters, float sample)
-{
-	int bucket = 0;
-	while (sample > letters[bucket].second)
-	{
-		bucket++;
-	}
-	assert(bucket < letters.size());
-	const auto letter = letters[bucket].first;
-	return letter;
-}
-
-GridResult populate(const vector<char> &grid, const unsigned int &w, const unsigned int &h, const vector<pair_lf> &letter_freqs, const string &word, const int pos, RNGBase &gen)
-{
-	GridResult res;
-	res.valid = false;
-	if (pos == -1)
-	{
-		const unsigned int grid_size = w * h;
-		vector<int> positions(grid_size);
-		for (int i = 0; i < grid_size; i++)
-		{
-			positions[i] = i;
-		}
-		while (!res.valid && !positions.empty())
-		{
-			int rand = gen() * positions.size();
-			int position = positions[rand];
-			positions.erase(positions.begin() + rand);
-			res = populate(grid, w, h, letter_freqs, word, position, gen);
-		}
-		return res;
-	}
-	if (grid[pos] != '\0')
-	{
-		return res;
-	}
-	vector<char> new_grid = grid;
-	new_grid[pos] = word[0];
-	if (word.length() == 1)
-	{
-		res.grid = new_grid;
-		res.valid = true;
-		for (int y = 0; y < h; y++)
-		{
-			for (int x = 0; x < w; x++)
-			{
-				if (res.grid[y * w + x] == '\0')
-				{
-					res.grid[y * w + x] = sample_letter(letter_freqs, gen());
-				}
-			}
-		}
-		return res;
-	}
-	const string new_word = word.substr(1);
-	vector<int> positions;
-	for (const EMove m : EMove())
-	{
-		int idx = move(pos, w, h, m);
-		if (idx == -1)
-		{
-			continue;
-		}
-		positions.push_back(idx);
-	}
-	while (!positions.empty() && !res.valid)
-	{
-		int rand = gen() * positions.size();
-		int position = positions[rand];
-		positions.erase(positions.begin() + rand);
-		res = populate(new_grid, w, h, letter_freqs, new_word, position, gen);
-	}
-	return res;
-}
-
-void print_grid(const vector<char> &grid, const int w, const int h)
-{
-	for (int y = 0; y < h; y++)
-	{
-		for (int x = 0; x < w; x++)
-		{
-			cout << grid[y * w + x] << ' ';
-		}
-		cout << endl;
-	}
-}
-
 int main()
 {
 	vector<pair_lf> letter_freqs;
@@ -260,7 +150,6 @@ int main()
 	//seed = -740863471;
 	cout << "seed: " << seed << endl;
 	mt19937 gen(seed);
-	SmartRNG<mt19937> g(gen);
 	vector<char> sampled_letters;
 	int w, h;
 	cin >> w >> h;
@@ -269,22 +158,24 @@ int main()
 	for (int i = 0; i < grid_size; i++)
 	{
 		const float sample = generate_canonical<float, 10>(gen);
-		const auto letter = sample_letter(bucketed_letters, sample);
+		int bucket = 0;
+		while (sample > bucketed_letters[bucket].second)
+		{
+			bucket++;
+		}
+		assert(bucket < 26);
+		const auto letter = bucketed_letters[bucket].first;
 		sampled_letters.push_back(letter);
 	}
-	//print_grid(sampled_letters, w, h);
-	vector<bool> visited(grid_size, false);
-	vector<char> grid(grid_size, '\0');
-	string word;
-	cin >> word;
-	GridResult res;
-	res = populate(grid, w, h, bucketed_letters, word, -1, g);
-	if (res.valid)
+	for (int y = 0; y < h; y++)
 	{
-		print_grid(res.grid, w, h);
-	} else {
-		cout << "solution for word '" << word << "' not found." << endl;
+		for (int x = 0; x < w; x++)
+		{
+			cout << sampled_letters[y * w + x] << ' ';
+		}
+		cout << endl;
 	}
+	vector<bool> visited(grid_size, false);
 	while (cin)
 	{
 		string word;
@@ -292,7 +183,7 @@ int main()
 		const int length = word.length();
 		if (length >= 4 && length < grid_size)
 		{
-			bool found = verify_word(res.grid, w, h, visited, word.c_str(), -1);
+			bool found = verify_word(sampled_letters, w, h, visited, word.c_str(), -1);
 			if (found)
 			{
 				cout << "found " << word << endl;
